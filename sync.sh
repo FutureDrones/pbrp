9861601e0b2ea8a7b8721e8aff528e758b5c799d
Wiley Lau <henloboii@protonmail.ch>
2021-04-04 19:18:34 +0800

    fucking rape with 48 core

--- sync.sh
+++ sync.sh
@@ -8,7 +8,7 @@ sudo chmod -R 0777 .
 
 repo init -q --no-repo-verify --depth=1 -u git://github.com/SHRP/platform_manifest_twrp_omni.git -b v3_10.0 -g default,-device,-mips,-darwin,-notdefault
 
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 30 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 48 || repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j 8
 
 curl -sL https://git.io/file-transfer | sh
 sudo cp -fpr transfer /bin/
